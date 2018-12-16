17a9575f992328e6a5d16314c1bc7e2cc2f8fa2b
Mikkel Lund <cybergolem@gmail.com>
2018-12-16 12:30:54 +0100

    Got the menu to change properly now and it's all good.. might not run in IE11, but who cares?!

--- cms.js
+++ cms.js
@@ -1,9 +1,56 @@
+var setPageProperties = {
+    
+    previous: "main",
+    
+    main: function() {
+        removeActive();
+        document.getElementById("menu_main").classList.add("active");
+        setTitle('Zhaky\'s page UNDER CONSTRUCTION');
+        setPageProperties.previous = "main";
+    },
+    
+    aboutMe: function() {
+        removeActive();
+        document.getElementById("menu_about_me").classList.add("active");
+        setTitle('About the guy they called Zhaky');
+        setPageProperties.previous = "about_me";
+    },
+    
+    blog: function() {
+        removeActive();
+        document.getElementById("menu_blog").classList.add("active");
+        setTitle('Things that occupy my head, space and time');
+        setPageProperties.previous = "blog";
+    },
+    
+    projects: function() {
+        removeActive();
+        document.getElementById("menu_projects").classList.add("active");
+        setTitle('Zhaky\'s mad inventions and activites');
+        setPageProperties.previous = "projects";
+    }
+    
+};
+
+function removeActive() {
+    var activeElm, elmId, classStr;
+    elmId = "menu_" + setPageProperties.previous;
+    activeElm = document.getElementById(elmId);
+    classStr = activeElm.className;
+    classStr = classStr.replace("active", "");
+    activeElm.className = classStr;
+}
+
 function changePage(id, file) {
 
-    var elm, xhttp;
+    var elm, xhttp, pageName, rmStr;
+    
+    pageName = file;
+    rmStr = pageName.slice(pageName.indexOf("."));
+    pageName = pageName.replace(rmStr, "");
+    setPageProperties[pageName]();
     
     elm = document.getElementById(id);
-    console.log(elm);
     
     xhttp = new XMLHttpRequest();
     
@@ -20,5 +67,5 @@ function changePage(id, file) {
 }
 
 function setTitle(title) {
-    document.getElementById("pageTitle").innerHTML = title;
+    document.getElementById("page_title").innerHTML = title;
 }
